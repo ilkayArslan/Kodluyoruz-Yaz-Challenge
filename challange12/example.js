c8a1cf15db188d37358b542d14efe2dbadbbc74b
İlkay <108187198+ilkayArslan@users.noreply.github.com>
2023-08-20 14:43:35 +0300

    Add files via upload

---
+++ challange12/example.js
@@ -0,0 +1,25 @@
+//Easy
+
+let oto = 60;
+let sumroad = 240;
+let time = 240/60;
+console.log(time + " saate alır")
+
+//Medium
+let tophayvan = 36;
+let topbacak = 100;
+let x;
+let koyun = x*4;
+let tavuk = 72-(2*x);
+x = (100-72)/2;
+console.log("koyun sayısı : " + x);
+console.log( tophayvan-x + "  tavuk sayısı");
+
+//Hard
+let a =102; // toplamhavuz miktarı
+let su1 = a/10; //1.musluk saatte doldurduğu miktar
+let su2 = a/15; //2.musluk saatte doldurduğu miktar
+let su3 = a/30;//3.musluk saatte boşalttığı miktar
+
+console.log(a/(su1+su2-su3))
+
