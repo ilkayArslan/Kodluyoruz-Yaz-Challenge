01384d751dc8e171cfc990ce7c6d871e9017c6fa
İlkay <108187198+ilkayArslan@users.noreply.github.com>
2023-09-02 23:07:33 +0300

    Add files via upload

---
+++ challange final/example.js
@@ -0,0 +1,12 @@
+let birinci =15;
+let ikinci =20;
+
+let hesaplama = 20/(20-15);
+console.log(hesaplama)
+
+//Medium
+
+let hedef =36;
+let ay = 12;
+
+console.log("her ay okunması gereken : " + hedef/ay)
